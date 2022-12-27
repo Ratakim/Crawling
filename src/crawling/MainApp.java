0ae660404b02292b73de3637d60bd07561ffa58a
Knowch-note03 <Knowch-note03@DESKTOP-73IAA1V>
2022-12-27 15:24:18 +0900

    코드 주석

--- src/crawling/MainApp.java
+++ src/crawling/MainApp.java
@@ -31,52 +31,51 @@ public class MainApp {
 		// 1. 가져오기전 시간 찍기
 		System.out.println(" Start Date : " + getCurrentData());
 
-		// 2. 가져올 HTTP 주소 세팅
+		// 2. 가져올 HTTP 주소 넣기
 		HttpPost http = new HttpPost("https://www.naver.com/");
 
-		// 3. 가져오기를 실행할 클라이언트 객체 생성
+		// 3. 클라이언트 객체 생성
 		HttpClient httpClient = HttpClientBuilder.create().build();
 
-		// 4. 실행 및 실행 데이터를 Response 객체에 담음
+		// 4. 데이터를 Response에 담음
 		HttpResponse response = httpClient.execute(http);
 
-		// 5. Response 받은 데이터 중, DOM 데이터를 가져와 Entity(http header(본문에 대한 정보 및 요청/응답)와 http body를 담은 클래스)에 담음
+		// 5. Response 받은 데이터 중, DOM(문서 객체 모델) 데이터를 가져와 Entity(http header(본문에 대한 정보 및 요청/응답)와 http body를 담은 클래스)에 담음
 		HttpEntity entity = response.getEntity();
 
-		// 6. Charset을 알아내기 위해 DOM의 컨텐트 타입을 가져와 담고 Charset을 가져옴
+		// 6. 데이터의 컨텐트 타입을 담고 Charset을 가져옴
 		ContentType contentType = ContentType.getOrDefault(entity);		
 
 		Charset charset = contentType.getCharset();
 
-		// 7. DOM 데이터를 한 줄씩 읽기 위해 Reader에 담음, byte타입으로 읽어지는 값을 char 타입으로 처리한뒤 String 즉 문자열로 저장할 수 있게 한다는 의미 
+		// 7. 데이터를 한 줄씩 읽기 위해 BufferedReader에 담기, byte타입으로 읽어지는 값을 char 타입으로 처리한뒤 String 즉 문자열로 저장할 수 있게 한다는 의미 
 		BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent(), charset));
 
-		// 8. 가져온 DOM 데이터를 담기위한 그릇, 그냥 String을 쓰면 값이 현저하게 느려짐
+		// 8. 가져온 데이터를 담을 StringBuffer, 그냥 String을 쓰면 값이 현저하게 느려짐
 		StringBuffer sb = new StringBuffer();
-
-		// 9. DOM 데이터 가져오기
+		
 		String line = "";
-
+		
+		// 9. br 내용 한 줄씩 읽어 들이는데 null이 아닐시 
 		while ((line = br.readLine()) != null) {
 
-			//.append 값 뒤에 붙힘
+			//.append -> 값 뒤에 붙힘
 			sb.append(line + "\n");
 
 		}
 
-		// 10. 가져온 DOM을 보자
-	    //System.out.println(sb.toString());
+		// 10. 가져온 데이터 값을 출력
+	    System.out.println(sb.toString());
 
-		// 11. Jsoup으로 파싱해보자.
+		//Jsoup으로 파싱
 		//Document doc = Jsoup.parse(sb.toString()); 
 		 
-		// 참고 - Jsoup에서 제공하는 Connect 처리		
+		//Connect 처리		
 		//Document doc2 = Jsoup.connect("https://www.naver.com/").get();
 	
-
 	    //System.out.println(doc2.data());
 
-		// 12. 얼마나 걸렸나 찍어보자
+		// 12. 얼마나 걸린지 체크
 		System.out.println(" End Date : " + getCurrentData());
 
 	}
